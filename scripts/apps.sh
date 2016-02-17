528e230235ee6e06c84badb5bfba69f373832914
Ben Knoll <knol0061@umn.edu>
2016-02-17 13:53:13 -0600

    Removed submodules, checkout and build projects instead

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -1,15 +1,12 @@
+git clone https://github.com/nlpie/nlptab.git
+git checkout tags/v1.1
 cd /home/vagrant/nlptab
-
 mvn clean install
-
 /usr/share/elasticsearch/bin/plugin install file:///home/vagrant/NLP-TAB/target/releases/nlptab-1.1.0.zip
 
+git clone https://github.com/nlpie/nlptab-webapp.git
 cd /home/vagrant/nlptab-webapp
-
 npm install
-
 gulp
-
 cp -R app/* /var/www/html/
-
 ufw allow http
