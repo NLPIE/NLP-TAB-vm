a78041e4f2fb1eeb03a610089ce7a5f089c2f45c
Ben Knoll <knol0061@umn.edu>
2016-02-17 15:26:31 -0600

    Use random ports.

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -4,9 +4,11 @@ cd /home/vagrant/nlptab
 mvn clean install
 /usr/share/elasticsearch/bin/plugin install file:///home/vagrant/NLP-TAB/target/releases/nlptab-1.1.0.zip
 
+cd /home/vagrant/
 git clone https://github.com/nlpie/nlptab-webapp.git
 cd /home/vagrant/nlptab-webapp
 npm install
 gulp
+sed -i.bak "s/esServer: 'localhost:9200'/esServer: 'localhost:58032'/g" app/config.js
 cp -R app/* /var/www/html/
 ufw allow http
